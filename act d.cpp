3b9013bb5e17a9933e1e5ca4692b549d02abf99d
Jhon Martin Barbosa <pandagamingvr2@gmail.com>
2024-11-30 23:44:51 +0800

    Add files via upload

---
+++ act d.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int n = 8; // Number of rows
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = i; j >= 1; j--) {
+            std::cout << j << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
